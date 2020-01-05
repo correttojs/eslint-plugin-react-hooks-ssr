836013af66c19ec574226013afcfc4655f62bd1f
Fabio Benedetti <fabio.benedetti@7-tv.de>
2020-01-05 16:14:37 +0100

    add unit tests

---
+++ tests/globalRule.js
@@ -0,0 +1,62 @@
+const globalRule = require("../lib/index");
+const RuleTester = require("eslint").RuleTester;
+
+const ruleTester = new RuleTester();
+
+ruleTester.run(
+  "react-hooks-global-ssr",
+  globalRule.rules["react-hooks-global-ssr"],
+  {
+    valid: [
+      {
+        code: "function useCustomHook(){ var x = window.innerHeight;}"
+      },
+      {
+        code: `function Comp(){ 
+            useEffect(function() {
+                var x = window.innerHeight;
+            })
+        }`
+      },
+      {
+        code: `function Comp(){ 
+            useCustomHook(function() {
+                var x = window.innerHeight;
+            })
+        }`
+      },
+      {
+        code: `function asyncComp(){ 
+            var x = window.innerHeight;
+        }`
+      },
+      {
+        code: `function testFunc(){ 
+            var x = window.innerHeight;
+        }`,
+        options: [{ allowFuncRegExp: /testFunc/ }]
+      }
+    ],
+
+    invalid: [
+      {
+        code: "function Comp(){ var x = window.innerHeight;}",
+        errors: [{ message: "Use of DOM globals is forbidden in this scope" }]
+      },
+      {
+        code: `function Comp(){ 
+            useState(function() {
+                var x = window.innerHeight;
+            })
+        }`,
+        errors: [{ message: "Use of DOM globals is forbidden in this scope" }]
+      },
+      {
+        code: `function syncComp(){ 
+            var x = window.innerHeight;
+        }`,
+        errors: [{ message: "Use of DOM globals is forbidden in this scope" }]
+      }
+    ]
+  }
+);
